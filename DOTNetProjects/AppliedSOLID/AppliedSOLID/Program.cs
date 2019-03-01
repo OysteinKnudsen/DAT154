e4aa0481409c358a9478b181401f75435e9efa99
OysteinKnudsen <oysteinknudsen96@gmail.com>
2019-03-01 14:45:13 +0100

    initial program before applying single responsibility

--- DOTNetProjects/AppliedSOLID/AppliedSOLID/Program.cs
+++ DOTNetProjects/AppliedSOLID/AppliedSOLID/Program.cs
@@ -10,6 +10,35 @@ namespace AppliedSOLID
     {
         static void Main(string[] args)
         {
+
+            //Ask user for information
+            Console.WriteLine("Welcome to my application");
+
+            Person user = new Person();
+
+            Console.WriteLine("What is your name?");
+            user.FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            user.LastName = Console.ReadLine();
+
+
+            //Check to see if first and last names are valid
+            if (string.IsNullOrWhiteSpace(user.FirstName))
+            {
+                Console.WriteLine("Invalid first name");
+                Console.ReadLine();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(user.LastName))
+            {
+                Console.WriteLine("Invalid last name");
+                Console.ReadLine();
+                return;
+            }
+
+
         }
     }
 }
