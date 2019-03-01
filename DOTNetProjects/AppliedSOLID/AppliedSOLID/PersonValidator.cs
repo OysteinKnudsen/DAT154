4d71a52681925e07ea12cca08067e70df62d513b
OysteinKnudsen <oysteinknudsen96@gmail.com>
2019-03-01 14:56:07 +0100

    Extracted validation functionality to own class

---
+++ DOTNetProjects/AppliedSOLID/AppliedSOLID/PersonValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedSOLID
+{
+    class PersonValidator
+    {
+        public static bool Validate(Person person)
+        {
+            //Check to see if first and last names are valid
+            if (string.IsNullOrWhiteSpace(person.FirstName))
+            {
+                Console.WriteLine("Invalid first name");
+                StandardMessages.EndApplication();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(person.LastName))
+            {
+                Console.WriteLine("Invalid last name");
+                StandardMessages.EndApplication();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
